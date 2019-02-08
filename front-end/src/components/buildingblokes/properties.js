3060415652f1b4a305235fd3a1261bb12c346bdb
The Broons <davidbrown090391@gmail.com>
2019-02-08 11:46:44 -0800

    mapping of houses fixed

--- front-end/src/components/buildingblokes/properties.js
+++ front-end/src/components/buildingblokes/properties.js
@@ -1,18 +1,31 @@
 import React from "react";
 import * as api from "../../utils/api";
+import {
+  Grid,
+  Cell,
+  Card,
+  CardText,
+  CardTitle,
+  CardActions,
+  Button
+} from "react-mdl";
 
 class Properties extends React.Component {
-  state = {
-    properties: [],
-    isLoaded: false
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      properties: [],
+      isLoaded: false
+    };
+  }
 
   callProperties = () => {
     api.fetchValues().then(response => {
       console.log("callProperties: ", response);
       this.setState(() => {
         return {
-          properties: response
+          properties: response,
+          isLoaded: true
         };
       });
       console.log("after SetState: ", this.state.properties);
@@ -21,10 +34,6 @@ class Properties extends React.Component {
 
   componentDidMount() {
     this.callProperties();
-    this.setState({
-      isLoaded: true,
-      properties: JSON
-    });
   }
 
   render() {
@@ -35,11 +44,40 @@ class Properties extends React.Component {
     } else {
       return (
         <div>
-          {this.state.properties.map((property, index) => (
-            <p key={index}>
-              Hello, {property.homeName} from {property.city}!
-            </p>
-          ))}
+          {this.state.properties.map(p => (
+            
+              <div>
+              <Grid className="demo-grid-2">
+                <Cell col={2}>
+                  <Card
+                    shadow={0}
+                    style={{ width: "320px", height: "320px", margin: "auto" }}
+                  >
+                    <CardTitle
+                      expand
+                      style={{
+                        color: "#fff",
+                        background:
+                          "url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #46B6AC"
+                      }}
+                      ><h3>{p.homeName}</h3></CardTitle>
+                    <CardText>
+                    {p.address.line1}, {p.address.city}, {p.address.state},{" "}
+                    {p.address.zip}
+                    </CardText>
+                    <CardActions border>
+                      <Button colored>View Updates</Button>
+                    </CardActions>
+                  </Card>
+                </Cell>
+                
+              </Grid>
+            </div>
+          ),
+        
+      
+            
+          )}
         </div>
       );
     }
--- front-end/src/components/pages/buy.js
+++ front-end/src/components/pages/buy.js
@@ -9,7 +9,7 @@ import {
   Button
 } from "react-mdl";
 import Map from "../buildingblokes/map.js";
-import Properties from "../buildingblokes/properties";
+import Properties from "../buildingblokes/properties.js";
 
 class Buy extends Component {
   state = {};
@@ -20,58 +20,8 @@ class Buy extends Component {
           <Cell col={8} style={{}}>
             <h1 style={{}}>Buy Page</h1>
             <Map />
-            <Properties />
           </Cell>
-          <Cell col={2}>
-            <Card
-              shadow={0}
-              style={{ width: "320px", height: "320px", margin: "auto" }}
-            >
-              <CardTitle
-                expand
-                style={{
-                  color: "#fff",
-                  background:
-                    "url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #46B6AC"
-                }}
-              >
-                Update
-              </CardTitle>
-              <CardText>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenan
-                convallis.
-              </CardText>
-              <CardActions border>
-                <Button colored>View Updates</Button>
-              </CardActions>
-            </Card>
-            ;
-          </Cell>
-          <Cell col={2}>
-            <Card
-              shadow={0}
-              style={{ width: "320px", height: "320px", margin: "auto" }}
-            >
-              <CardTitle
-                expand
-                style={{
-                  color: "#fff",
-                  background:
-                    "url(http://www.getmdl.io/assets/demos/dog.png) bottom right 15% no-repeat #46B6AC"
-                }}
-              >
-                Update
-              </CardTitle>
-              <CardText>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenan
-                convallis.
-              </CardText>
-              <CardActions border>
-                <Button colored>View Updates</Button>
-              </CardActions>
-            </Card>
-            ;
-          </Cell>
+          <Properties />
         </Grid>
       </div>
     );
